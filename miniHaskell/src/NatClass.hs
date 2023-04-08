2075ce00d40f3c31f0c43feea507e95525670efa
lowLevelGod <petrepopa19@gmail.com>
2023-04-09 00:24:53 +0300

    done natclass but sub returns 1 instead of 0 for equal numbers

--- miniHaskell/src/NatClass.hs
+++ miniHaskell/src/NatClass.hs
@@ -6,6 +6,8 @@ import qualified GHC.Natural as Natural
 import MyPrelude
 import BoolClass
 import MaybeClass
+import PairClass (fst, PairClass (pair), CPair)
+import PairClass (snd)
  
 -- | The class of Natural-like types (types having a notion of
 -- 'zero', 'succ'essor, and 'iter'ation).
@@ -41,70 +43,85 @@ isZero n = not (CBool (\x y -> iter (const y) x n))
 
 -- | Usual natural numbers addition
 add :: NatClass n => n -> n -> n
-add = undefined
+add = iter succ
 
 -- >>> add one one :: Natural.Natural
 -- 2
 
 -- | Usual natural numbers multiplication
 mul :: NatClass n => n -> n -> n
-mul = undefined
+mul x = iter (add x) zero
 
 -- >>> mul one one :: Natural.Natural
 -- 1
 
 -- | Usual natural numbers exponentiation (@exp m n@ is @m ^ n@)
 exp :: NatClass n => n -> n -> n
-exp = undefined
+exp x = iter (mul x) one
 
 -- >>> exp (add one one) (add one one) :: Natural.Natural
 -- 4
 
 -- | Predecessor of a natural number ('nothing' for 'zero')
 pred :: NatClass n  => n -> CMaybe n
-pred = undefined
+pred n = ite (isZero n) nothing (just (snd nonZero))
+        where nonZero = iter skipOne (pair true zero) n
+              skipOne :: (NatClass n) => CPair CBool n -> CPair CBool n
+              skipOne p = ite (fst p) (pair false (snd p)) (pair false (succ (snd p)))
 
 -- >>> pred zero :: CMaybe Natural.Natural
 -- CNothing
 
+
 -- | Difference between natural numbers as a 'MaybeClass' ('nothing' if first is smaller)
 sub :: NatClass n  => n -> n -> CMaybe n
-sub = undefined
+sub x y 
+  = ite (isZero (fromMaybe zero res1)) secondCase res1
+    where
+          res1 = just (iter f x y)
+          res2 = just (iter f (succ x) y)
+          f p = fromMaybe zero (pred p)  
+          secondCase = ite (isZero (fromMaybe zero res2)) nothing res2
 
 -- >>> sub (exp (add one one) (add one one)) one :: CMaybe Natural.Natural
 -- CJust 3
 
 -- | (Strictly-)Less-Than predicate for natural numbers.
 lt :: NatClass n => n -> n -> CBool 
-lt = undefined
+lt x y = maybe true (const false) (sub x y)
 
 -- >>> lt (one :: Natural.Natural) one
 -- CFalse
 
 -- | (Strictly-)Greater-Than predicate for natural numbers.
 gt :: NatClass n => n -> n -> CBool 
-gt = undefined
+gt x y = maybe true (const false) (sub y x)
 
 -- >>> gt (one :: Natural.Natural) one
 -- CFalse
 
 -- | Greater-Than-or-Equal-To predicate for natural numbers.
 gte :: NatClass n => n -> n -> CBool 
-gte = undefined
+gte x y = maybe true (const cond) res
+      where res = sub y x 
+            cond = fromMaybe false (maybeFMap isZero res)
 
 -- >>> gte (zero :: Natural.Natural) zero
 -- CTrue
 
 -- | Less-Than-or-Equal-To predicate for natural numbers.
 lte :: NatClass n => n -> n -> CBool 
-lte = undefined
+lte x y = maybe true (const cond) res
+      where res = sub x y 
+            cond = fromMaybe false (maybeFMap isZero res)
 
 -- >>> lte (zero :: Natural.Natural) zero
 -- CTrue
 
 -- | Equality predicate for natural numbers.
 eq :: NatClass n => n -> n -> CBool 
-eq = undefined
+eq x y = fromMaybe false (maybeFMap isZero res)
+      where res = sub x y 
 
 -- >>> eq (zero :: Natural.Natural) zero
 -- CTrue
@@ -114,7 +131,7 @@ eq = undefined
 
 -- | Returns the greater between its two arguments
 max :: NatClass n => n -> n -> n
-max = undefined
+max x y = ite (gt x y) x y 
 
 -- >>> max (zero :: Natural.Natural) one
 -- 1
@@ -123,8 +140,8 @@ newtype CNat = CNat { getCNat :: forall a . (a -> a) -> a -> a }
 
 instance NatClass CNat where
   iter f i n = getCNat n f i
-  zero = undefined
-  succ = undefined
+  zero = CNat (\f x -> x)
+  succ n = CNat (\f x -> f (getCNat n f x))
 
 -- | converting between different instances of 'NatClass'
 fromNatClass :: (NatClass n, NatClass m) => n -> m
